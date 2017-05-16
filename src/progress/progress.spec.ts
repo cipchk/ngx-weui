d7138437ac63bdb62cd860d11566388affa0180c
cipchk <cipchk@gmail.com>
2017-05-16 13:53:52 +0800

    add unit test.

---
+++ src/progress/progress.spec.ts
@@ -0,0 +1,60 @@
+import { Subscriber } from 'rxjs/Rx';
+import { Component, ViewChild, DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentFixture, TestBed, fakeAsync, tick, async, inject } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { ProgressModule, ProgressComponent } from '../progress';
+
+describe('Component: Progress', () => {
+
+    let fixture: ComponentFixture<TestProgressComponent>;
+    let context: TestProgressComponent;
+    let comp: ProgressComponent;
+    let barEl: HTMLElement;
+
+    beforeEach(fakeAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [TestProgressComponent],
+            imports: [ProgressModule.forRoot(), FormsModule ]
+        });
+        fixture = TestBed.createComponent(TestProgressComponent);
+        context = fixture.componentInstance;
+        fixture.detectChanges();
+        comp = fixture.debugElement.query(By.css('weui-progress')).componentInstance as ProgressComponent;
+        barEl = fixture.debugElement.query(By.css('.weui-progress__inner-bar')).nativeElement as HTMLElement;
+        tick();
+    }));
+
+    it('should define default values', () => {
+        expect(comp._value).toBe(0);
+        expect(comp.canCancel).toBe(true);
+    });
+
+    it('should value between 0 and 100', () => {
+        const VALUES = [
+            { v: 50, r: 50 },
+            { v: 101, r: 100 },
+            { v: -10, r: 0 }
+        ];
+        for (let item of VALUES) {
+            comp.value = item.v;
+            expect(comp._value).toBe(item.r);
+        }
+    });
+
+    it('should set "width" style value', () => {
+        comp.value = 10;
+        fixture.detectChanges();
+        expect(barEl.style.width).toBe('10%');
+
+        comp.value = 99;
+        fixture.detectChanges();
+        expect(barEl.style.width).toBe('99%');
+    });
+
+});
+
+@Component({ template: `<weui-progress></weui-progress>` })
+class TestProgressComponent { }
--- src/tab/navbar.component.ts
+++ src/tab/navbar.component.ts
@@ -8,7 +8,9 @@ import { BarComponent } from './bar.component';
     selector: 'weui-navbar',
     template: `
     <div class="weui-navbar">
-        <div class="weui-navbar__item" [ngClass]="{'weui-bar__item_on': item.active}"
+        <div class="weui-navbar__item" 
+            [ngClass]="{'weui-bar__item_on': item.active}"
+            [class.disabled]="item.disabled"
             *ngFor="let item of tabs" (click)="item.active=true">{{item.heading}}</div>
     </div>
     <div class="weui-tab__panel"><ng-content></ng-content></div>
