f42b3295bc7ce0b313f3865fba668325ba037da3
cipchk <cipchk@gmail.com>
2017-08-31 23:53:20 +0800

    feat(check-list) fix ExpressionChangedAfterItHasBeenCheckedError bug.

--- src/form/check.directive.ts
+++ src/form/check.directive.ts
@@ -9,19 +9,22 @@ import { Directive, Input } from '@angular/core';
 })
 export class ChecklistDirective {
     @Input('weui-checklist') targetArray: any[];
-    @Input('weui-value') value: any;
 
     checked: boolean = false;
-    ngAfterViewInit() {
-        this.checked = this.targetArray.indexOf(this.value) !== -1;
+    private _value: any;
+
+    @Input('weui-value')
+    set value(val: any) {
+        this._value = val;
+        this.checked = this.targetArray.indexOf(val) !== -1;
     }
 
     _change($event: any) {
         if ($event.target.checked) {
-            this.targetArray.push(this.value);
+            this.targetArray.push(this._value);
         }
         else {
-            this.targetArray.splice(this.targetArray.indexOf(this.value), 1);
+            this.targetArray.splice(this.targetArray.indexOf(this._value), 1);
         }
     }
 }
