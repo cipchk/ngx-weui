def2df84b1502332df03c8a313ac66040f54abbf
cipchk <cipchk@qq.com>
2017-11-13 12:29:44 +0800

    feat(mask): support customize propery. close #50

--- demo/src/app/example/mask/mask.component.ts
+++ demo/src/app/example/mask/mask.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { MaskComponent } from "ngx-weui/mask";
+import { MaskComponent } from 'ngx-weui/mask';
 
 @Component({
     selector: 'example-mask',
@@ -8,7 +8,11 @@ import { MaskComponent } from "ngx-weui/mask";
 })
 export class DemoMaskComponent {
 
+    loading = false;
+
     @ViewChild('mask') mask: MaskComponent;
+
+    @ViewChild('maskContent') maskContent: MaskComponent;
 
     onShow(isNormal: boolean = false) {
         this.mask.show().subscribe(() => {
@@ -18,8 +22,23 @@ export class DemoMaskComponent {
         if (!isNormal) {
             setTimeout(() => {
                 this.mask.hide();
-            }, 1000* 2);
+            }, 1000 * 2);
         }
+    }
+
+    onShowLoading() {
+        this.mask.show();
+        this.loading = true;
+        setTimeout(() => {
+            this.mask.hide();
+        }, 1000 * 2);
+    }
+
+    showContent() {
+        this.maskContent.show();
+        setTimeout(() => {
+            this.maskContent.hide();
+        }, 1000 * 2);
     }
 
 }
--- src/mask/mask.component.ts
+++ src/mask/mask.component.ts
@@ -5,7 +5,16 @@ import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'weui-mask',
-    template: `<div class="weui-mask" [ngClass]="{'weui-mask--visible': _shown }" (click)="hide(true)"></div>`,
+    template: `
+    <div class="weui-mask" [ngClass]="{'weui-mask__visible': _shown }" (click)="hide(true)">
+        <div class="weui-mask__content" [ngClass]="placement !== 'none' ? 'weui-mask__' + placement : ''">
+            <div [ngStyle]="{'background-color':bg}">
+                <div *ngIf="loading"><i class="weui-loading weui-icon_toast"></i></div>
+                <ng-content></ng-content>
+            </div>
+        </div>
+    </div>
+    `,
     styleUrls: [ './mask.scss' ],
     encapsulation: ViewEncapsulation.None
 })
@@ -16,6 +25,33 @@ export class MaskComponent implements OnDestroy {
      * @type {boolean}
      */
     @Input() backdrop: boolean = false;
+
+    /**
+     * 内容方向（默认：vertical）
+     * + `top`: 顶部居中
+     * + `bottom`: 底部居中
+     * + `vertical-left`: 垂直居左
+     * + `vertical`: 垂直居中
+     * + `vertical-right`: 垂直居右
+     * + `none`: 无
+     *
+     * @type {string}
+     */
+    @Input() placement: 'top' | 'bottom' | 'vertical' | 'vertical-left' | 'vertical-right' | 'none' = 'vertical';
+
+    /**
+     * 内容背景色（默认：无）
+     *
+     * @type {string}
+     */
+    @Input() bg = '';
+
+    /**
+     * 内容为Loading效果（默认：false）
+     *
+     * @type {boolean}
+     */
+    @Input() loading = false;
 
     /**
      * 关闭回调
