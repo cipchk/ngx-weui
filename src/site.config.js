4c3cfce51c8f325bbd02c44f4f5b7794ec4baf19
cipchk <cipchk@qq.com>
2019-05-30 10:22:01 +0700

    chore: build

--- src/site.config.js
+++ src/site.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    theme: {
-        github: 'https://github.com/cipchk/ngx-weui',
-        defaultLang: 'zh-CN',
-        langs: [ 'zh-CN', 'en-US' ]
+  theme: {
+    github: 'https://github.com/cipchk/ngx-weui',
+    defaultLang: 'zh-CN',
+    langs: ['zh-CN', 'en-US'],
+  },
+  modules: [
+    {
+      name: 'components',
+      src: './components/',
+      template: {
+        meta: './src/templates/meta.ts',
+      },
+      dist: './src/app/routes/components',
+      ignores: ['utils', 'style'],
+      example: {
+        src: './src/app/example',
+        template: './src/templates/example.ts',
+      },
     },
-    modules: [
-        {
-            name: 'components',
-            src: './components/',
-            template: {
-                meta: './src/templates/meta.ts'
-            },
-            dist: './src/app/routes/components',
-            ignores: [ 'utils', 'style' ],
-            example: {
-                src: './src/app/example',
-                template: './src/templates/example.ts'
-            }
-        },
-        {
-            name: 'docs',
-            src: './docs/',
-            template: {
-                meta: './src/templates/meta.ts'
-            },
-            dist: './src/app/routes/docs'
-        }
-    ]
+    {
+      name: 'docs',
+      src: './docs/',
+      template: {
+        meta: './src/templates/meta.ts',
+      },
+      dist: './src/app/routes/docs',
+    },
+  ],
 };
