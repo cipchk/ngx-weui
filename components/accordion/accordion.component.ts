4a6f8884b781190de880f67ceee24318f1854c9c
cipchk <cipchk@qq.com>
2020-07-08 19:55:54 +0800

    chore: update

--- components/accordion/accordion.component.ts
+++ components/accordion/accordion.component.ts
@@ -1,23 +1,124 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'weui-accordion-panel',
-  exportAs: 'weuiAccordionPanel',
-  template: ``,
-  host: {},
-  preserveWhitespaces: false,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
-})
-export class AccordionPanelComponent {}
+import { ChangeDetectionStrategy, Component, EventEmitter, Host, Input, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { AnimateType, InputBoolean } from 'ngx-weui/core';
+import { AccordionConfig } from './accordion.config';
 
 @Component({
   selector: 'weui-accordion',
   exportAs: 'weuiAccordion',
   template: ` <ng-content></ng-content> `,
-  host: {},
+  host: {
+    '[attr.aria-multiselectable]': 'collapsible',
+  },
   preserveWhitespaces: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class AccordionComponent {}
+export class AccordionComponent {
+  /**
+   * 是否可折叠，`true` 表示同时所有都允许展开，`false` 表示同时只允许一个展开，默认：`false`
+   */
+  @Input() @InputBoolean() collapsible: boolean = false;
+  /**
+   * 自动展开第一次，默认：`true`
+   */
+  @Input() @InputBoolean() activeFirst: boolean = true;
+  /**
+   * 动画类型，`none` 无动画，`slide` 滑动，默认：`slide`
+   */
+  @Input() _animate: AnimateType = 'slide';
+
+  /**
+   * 展开时回调，参数为面板下标。
+   */
+  @Output() readonly select = new EventEmitter<number>();
+
+  private panels: AccordionPanelComponent[] = [];
+
+  constructor(config: AccordionConfig) {
+    Object.assign(this, config);
+  }
+
+  _add(item: AccordionPanelComponent): void {
+    this.panels.push(item);
+    if (this.panels.length === 1 && this.activeFirst) {
+      item.active = true;
+    }
+  }
+
+  _remove(item: AccordionPanelComponent): void {
+    this.panels.splice(this.panels.indexOf(item), 1);
+  }
+
+  _index(item: AccordionPanelComponent): number {
+    return this.panels.indexOf(item);
+  }
+
+  _closeOthers(cur: AccordionPanelComponent): void {
+    if (this.collapsible) {
+      return;
+    }
+
+    this.panels.forEach(panel => {
+      if (!panel.disabled && panel !== cur) {
+        panel.active = false;
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'weui-accordion-panel',
+  exportAs: 'weuiAccordionPanel',
+  template: `
+    <div role="tab" (click)="_toggle()"><ng-content select="[heading]"></ng-content></div>
+    <div role="tabpanel" class="weui-accordion-content"><ng-content></ng-content></div>
+  `,
+  host: {
+    '[class.weui-accordion-panel-disabled]': 'disabled',
+    '[class.weui-accordion-active]': 'active',
+  },
+  preserveWhitespaces: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class AccordionPanelComponent implements OnInit, OnDestroy {
+  /**
+   * 是否禁止
+   */
+  @Input() @InputBoolean() disabled: boolean = false;
+
+  private _active: boolean = false;
+
+  /**
+   * 是否展开
+   */
+  @Input()
+  @InputBoolean()
+  get active(): boolean {
+    return this._active;
+  }
+
+  set active(value: boolean) {
+    this._active = value;
+    if (value) {
+      this.accordion._closeOthers(this);
+    }
+  }
+
+  constructor(@Host() protected accordion: AccordionComponent) {}
+
+  ngOnInit(): void {
+    this.accordion._add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.accordion._remove(this);
+  }
+
+  _toggle(): void {
+    if (!this.disabled) {
+      this.active = !this.active;
+      this.accordion.select.emit(this.accordion._index(this));
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,17 +6,14 @@ import { AqmModule } from 'angular-qq-maps';
 import { CountdownModule } from 'ngx-countdown';
 import { GesturePasswordModule } from 'ngx-gesture-password';
 import { ToastrModule } from 'ngx-toastr';
-// import { SHARED_WEUI_MODULES } from './shared-weui.module';
-// tslint:disable-next-line: import-blacklist
-import { WeUiModule } from 'ngx-weui';
+import { SHARED_WEUI_MODULES } from './shared-weui.module';
 
 import { DocsNavComponent } from './docs-nav/docs-nav.component';
 import { EditButtonComponent } from './edit-button/edit-button.component';
 import { ThemeBtnComponent } from './theme-btn/theme-btn.component';
 
 const COMPONENTS = [DocsNavComponent, EditButtonComponent, ThemeBtnComponent];
-const SHARED_WEUI_MODULES: any[] = [];
-const THIDS = [CountdownModule, ToastrModule, GesturePasswordModule, AqmModule, WeUiModule];
+const THIDS = [CountdownModule, ToastrModule, GesturePasswordModule, AqmModule];
 
 @NgModule({
   imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule, ...SHARED_WEUI_MODULES, ...THIDS],
